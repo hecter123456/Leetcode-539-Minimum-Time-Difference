a75fe35838e32b537c586fc2d7f66e3b753cb2af
hecter_wu <hecter_wu@transcend.com.tw>
2017-07-17 16:09:23 +0800

    pass testcase: testCrossHour and testCrossDay by return total

--- Minimum Time Difference.py
+++ Minimum Time Difference.py
@@ -5,11 +5,24 @@ class unitest(unittest.TestCase):
         Input = ["00:00","00:00"]
         Ans = 0
         self.assertEqual(Solution().findMinDifference(Input),Ans);
+    def testCrossHour(self):
+        Input = ["23:00","21:20"]
+        Ans = 100
+        self.assertEqual(Solution().findMinDifference(Input),Ans);
+    def testCrossDay(self):
+        Input = ["23:59","01:20"]
+        Ans = 81
+        self.assertEqual(Solution().findMinDifference(Input),Ans);
 
 class Solution():
     def findMinDifference(self, timePoints):
         if(timePoints[0] == timePoints[1]):
             return 0
+        hour = int(timePoints[0][:2]) - int(timePoints[1][:2])
+        minute = int(timePoints[0][3:]) - int(timePoints[1][3:])
+        total = abs(hour*60 + minute)
+        total = min(total,abs(total-1440))
+        return total
 
 if __name__ == '__main__':
     unittest.main()
