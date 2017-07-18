a7dedb09dcc02098bf35114108d815a295a929d4
hecter_wu <hecter_wu@transcend.com.tw>
2017-07-18 10:34:17 +0800

    pass testcase: testFourInput by using sort

--- Minimum Time Difference.py
+++ Minimum Time Difference.py
@@ -1,28 +1,41 @@
 import unittest
+import sys
 
 class unitest(unittest.TestCase):
     def testSameTime(self):
         Input = ["00:00","00:00"]
         Ans = 0
-        self.assertEqual(Solution().findMinDifference(Input),Ans);
+        self.assertEqual(Solution().findMinDifference(Input),Ans)
     def testCrossHour(self):
         Input = ["23:00","21:20"]
         Ans = 100
-        self.assertEqual(Solution().findMinDifference(Input),Ans);
+        self.assertEqual(Solution().findMinDifference(Input),Ans)
     def testCrossDay(self):
         Input = ["23:59","01:20"]
         Ans = 81
-        self.assertEqual(Solution().findMinDifference(Input),Ans);
+        self.assertEqual(Solution().findMinDifference(Input),Ans)
+    def testFourInput(self):
+        Input = ["23:59","01:20","23:00","21:20"]
+        Ans = 59
+        self.assertEqual(Solution().findMinDifference(Input),Ans)
 
 class Solution():
     def findMinDifference(self, timePoints):
-        if(timePoints[0] == timePoints[1]):
-            return 0
-        hour = int(timePoints[0][:2]) - int(timePoints[1][:2])
-        minute = int(timePoints[0][3:]) - int(timePoints[1][3:])
-        total = abs(hour*60 + minute)
-        total = min(total,abs(total-1440))
-        return total
+        def GetTotal(timePointsA,timePointsB):
+            if(timePointsA == timePointsB):
+                return 0
+            hour = int(timePointsA[:2]) - int(timePointsB[:2])
+            minute = int(timePointsA[3:]) - int(timePointsB[3:])
+            total = abs(hour*60 + minute)
+            total = min(total,abs(total-1440))
+            return total
+        timePoints.sort(key = lambda a : (int(a[:2]),int(a[3:])))
+        Ans = sys.maxsize
+        for indexA in range(len(timePoints)-1):
+            Ans = min(Ans,GetTotal(timePoints[indexA],timePoints[indexA+1]))
+        Ans = min(Ans,GetTotal(timePoints[0],timePoints[len(timePoints)-1]))
+        return Ans
+        
 
 if __name__ == '__main__':
     unittest.main()
